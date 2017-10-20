008717d35daf687282a6d685fb2206cf3d83bcd6
10041 <andreyoleksyuk004@gmail.com>
2017-10-20 11:14:41 +0300

    all zd kr 8

---
+++ client.js
@@ -0,0 +1,37 @@
+const net = require('net');
+const mix = require('./mixArray');
+const qa = require("./qa");
+const port = 8124;
+
+const client = new net.Socket();
+
+client.setEncoding('utf8');
+
+let questions = mix.mixArray(qa.questions);
+let currentQuestion;
+
+client.connect(port, function() {
+  console.log('Connected');
+  client.write(qa.qa);
+  client.once("data", data => {
+    interview();
+  })
+});
+
+const interview = () => {
+	currentQuestion = questions.pop();
+	client.write(currentQuestion.question);
+	client.once("data", answer => {
+		console.log(
+			answer === currentQuestion.answer ? "Right answer" : "Wrong answer"
+		);
+		console.log(`Question is ${currentQuestion.question}`);
+		console.log(`Right answer is ${currentQuestion.answer}`);
+		console.log(`Server answer is ${answer}\n\n`);
+		questions.length !== 0 ? interview.call() : client.destroy();
+	});
+};
+
+client.on('close', () => {
+  console.log('Connection closed');
+});
